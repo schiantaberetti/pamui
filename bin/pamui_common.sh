c0c6d39c799c6429d07ce18d30f44dce66de2e56
Baldo <baldo@lilik.it>
2012-04-16 22:08:36 +0200

    refactored

---
+++ bin/pamui_common.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+source pamui_errors.sh
+supported_pmfs=( pacman yum apt-get )
+
+echo_status_info(){
+	if [ $# -gt 0 ];then
+		echo "$0: $1"
+	fi
+}
+detect_pmf(){
+#pmf = packet manager frontend
+	PMF="NOT_FOUND"
+	n_pfms=${#supported_pmfs[*]};
+	for((i=0;i<$n_pfms;i++)); do  
+	#${supported_pmfs[$i]} --help # PMF=${supported_pmfs[$i]}
+		type ${supported_pmfs[$i]} >/dev/null 2>&1 && { PMF=${supported_pmfs[$i]}; }
+	done
+	echo $PMF
+}
+exec_as_root(){
+	if [ $# -gt 0 ];then
+		type sudo >/dev/null 2>&1 && sudo_present=true
+		if [ $sudo_present ];then
+			sudo "$1"
+		else
+			echo_status_info "sudo not found. Now using su.."
+			su -c " $1";
+		fi
+	else
+		err_few_parms;
+	fi
+}
+#safe_exec(){
+#	if [ $# -gt 0 ];then
+#		if [ -f $1 ];then
+#			if [ -x $1 ];then
+#				zenity --info --text="$1"
+#				source $1
+#				echo 0
+#			else
+#				echo 1
+#			fi;
+#		else
+#			echo 1	
+#		fi
+#	else
+#		echo 1 #err_few_parms;
+#	fi
+#}
---
+++ bin/pamui_pacman.sh
@@ -0,0 +1,62 @@
+#/bin/bash
+source pamui_errors.sh
+source pamui_common.sh
+search_package(){
+	if [ $# -gt 0 ];then
+		query=$1
+		for((i=2;i<$#;i++)); do
+			query="$query ${i}"
+		done
+		pacman -Ss $query;
+	else
+		err_few_parms;
+	fi
+}
+install_packages(){
+	if [ $# -gt 0 ];then
+		query=$1
+		for((i=2;i<$#;i++)); do
+			query="$query ${i}"
+		done
+		exec_as_root "pacman -S $query";
+	else
+		err_few_parms;
+	fi
+}
+remove_packages(){
+	if [ $# -gt 0 ];then
+		query=$1
+		for((i=2;i<$#;i++)); do
+			query="$query ${i}"
+		done
+		exec_as_root "pacman -R $query";
+	else
+		err_few_parms;
+	fi
+}
+purge_packages(){
+	if [ $# -gt 0 ];then
+		query=$1
+		for((i=2;i<$#;i++)); do
+			query="$query ${i}"
+		done
+		exec_as_root "pacman -R --nosave $query";
+	else
+		err_few_parms;
+	fi
+}
+update_db(){
+	exec_as_root "pacman -Syy"
+}
+upgrade_packages(){
+	exec_as_root "pacman -Su"
+}
+search_for_file(){
+	if [ $# -gt 0 ];then
+		pacman -Qo $1;
+	else
+		err_few_parms;
+	fi
+}
+
+
